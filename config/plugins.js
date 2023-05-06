c84d378fe41dcde07ac63fad903d1a0d77779ec3
asthapit38 <31070366+asthapit38@users.noreply.github.com>
2023-05-06 13:12:28 +0545

    removed env folder from config

--- config/plugins.js
+++ config/plugins.js
@@ -14,21 +14,4 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  "location-field": {
-    enabled: true,
-    config: {
-      fields: ["photo", "rating"], // optional
-      googleMapsApiKey: "your google api key", //You need to enable "Autocomplete API" and "Places API" in your Google Cloud Console
-    },
-  },
-  "vercel-deploy": {
-    enabled: true,
-    config: {
-      deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_1uoPtxybcCyQGTs0ENMqZPj92GFc/EJW2c5rm42",
-      apiToken: "FDZgWyhDpDEP4won2xhtn5wA",
-      appFilter: "icpl-backend",
-      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
-    },
-  },
 });
