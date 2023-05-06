03b2996a0273852ee00e40d1600f7f4293fc4757
asthapit38 <31070366+asthapit38@users.noreply.github.com>
2023-05-06 11:31:26 +0545

    fixed database and server

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: "cloudinary",
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
         upload: {},
@@ -19,6 +19,16 @@ module.exports = ({ env }) => ({
     config: {
       fields: ["photo", "rating"], // optional
       googleMapsApiKey: "your google api key", //You need to enable "Autocomplete API" and "Places API" in your Google Cloud Console
+    },
+  },
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_1uoPtxybcCyQGTs0ENMqZPj92GFc/EJW2c5rm42",
+      apiToken: "FDZgWyhDpDEP4won2xhtn5wA",
+      appFilter: "icpl-backend",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
   },
 });
